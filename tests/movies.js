00a10fa0db8a14c7f5134e2e33f0cc4463f41072
Felipe Koblinger <felipe.koblinger@gmail.com>
2018-10-20 03:49:37 -0300

    Adiciona testes completos.

--- tests/movies.js
+++ tests/movies.js
@@ -1,45 +1,73 @@
-const { test, todo } = QUnit;
+const { test } = QUnit;
 
-todo("generateCardHtml(object) deve retornar html sem nenhum undefined", assert => {
-    
+test("generateCardHtml(object) deve retornar html sem nenhum undefined", assert => {
+    var movie = { poster_path: 'a', overview: 'a', title: 'title', original_title: 'original-title',
+        release_date: 'release-date', popularity: '10' };
+    var html = generateCardHtml(movie);
+    assert.equal(html.indexOf('undefined'), -1, "Não retornou nenhum undefined.")
 });
 
-todo("generateCardHtml(object) deve retornar conteúdo com undefined quando o objeto não estiver completo", assert => {
-
+test("generateCardHtml(object) deve retornar conteúdo com undefined quando o objeto não estiver completo", assert => {
+    var movie = { poster_path: 'a', overview: 'a', title: 'title', original_title: 'original-title',
+        release_date: 'release-date' };
+    var html = generateCardHtml(movie);
+    assert.notEqual(html.indexOf('undefined'), -1, "Retornou undefined.")
 });
 
-todo("generateCardHtml(object) deve retornar false quando não houver parâmetros", assert => {
-
+test("generateCardHtml(object) deve retornar false quando não houver parâmetros", assert => {
+    var html = generateCardHtml();
+    assert.notOk(html, "Retornou false.")
 });
 
-todo("searchValidations(search) deve retornar mensagem de erro quando estiver vazio", assert => {
 
+test("searchValidations(search) deve retornar mensagem de erro quando tiver vazio", assert => {
+    var errorMessage = searchValidations('');
+    assert.equal(errorMessage, "Deve ter pelo menos duas letras.", "Retornou mensagem de erro.");
 });
 
-todo("searchValidations(search) deve retornar mensagem de erro quando tiver 1 letra", assert => {
-
+test("searchValidations(search) deve retornar mensagem de erro quando tiver 1 letra", assert => {
+    var errorMessage = searchValidations('a');
+    assert.equal(errorMessage, "Deve ter pelo menos duas letras.", "Retornou mensagem de erro.");
 });
 
-todo("searchValidations(search) deve retornar true quando tiver 2 letras", assert => {
-
+test("searchValidations(search) deve retornar true quando tiver 2 letras", assert => {
+    var errorMessage = searchValidations('');
+    assert.ok(errorMessage, "Ok");
 });
 
-todo("searchValidations(search) deve retornar true quando tiver 29 letras", assert => {
-
+test("searchValidations(search) deve retornar true quando tiver 29 letras", assert => {
+    var errorMessage = searchValidations('1234567890abcdefghij123456789');
+    assert.ok(errorMessage, "Ok");
 });
 
-todo("searchValidations(search) deve retornar true quando tiver 30 letras", assert => {
-
+test("searchValidations(search) deve retornar true quando tiver 30 letras", assert => {
+    var errorMessage = searchValidations('1234567890abcdefghij1234567890');
+    assert.ok(errorMessage, "Ok");
 });
 
-todo("searchValidations(search) deve retornar mensagem de erro quando tiver 31 letras", assert => {
-
+test("searchValidations(search) deve retornar mensagem de erro quando tiver 31 letras", assert => {
+    var errorMessage = searchValidations('1234567890abcdefghij12345678901');
+    assert.equal(errorMessage, "Deve ter no máximo 30 letras.", "Retornou mensagem de erro.");
 });
 
-todo("fetchMovies(string, callback) deve retornar objeto json com resultados", assert => {
-
+test("fetchMovies(string, callback) deve retornar objeto json com resultados", assert => {
+    var done = assert.async();
+    fetchMovies('ra', function(data) {
+        assert.ok(data.results.length > 0, "Resultados obtidos.");
+        done();
+    }, function() {
+        assert.notOk(true, "Houve erro de comunicação com a API.");
+        done();
+    });
 });
 
-todo("fetchMovies(string, callback) deve retornar objeto json de resultados vazio quando não encontrar o filme", assert => {
-
+test("fetchMovies(string, callback) deve retornar objeto json de resultados vazio quando não encontrar o filme", assert => {
+    var done = assert.async();
+    fetchMovies('120381209381287481274sdasdasd1', function(data) {
+        assert.equal(0, data.results.length, "Nenhum filme encontrado.");
+        done();
+    }, function() {
+        assert.notOk(true, "Houve erro de comunicação com a API.");
+        done();
+    });
 });
